8111ed2db1b82e166371c483b236c7687ad9349d
Johnvir <johnvir.singh.khattar@sap.com>
2020-03-05 17:58:55 +0100

    julea

--- src/mpi/romio/adio/ad_julea/ad_julea_open.c
+++ src/mpi/romio/adio/ad_julea/ad_julea_open.c
@@ -9,47 +9,14 @@
 
 void ADIOI_JULEA_Open(ADIO_File fd, int *error_code)
 {
-    int perm, amode;
-    mode_t old_mask;
     static char myname[] = "ADIOI_JULEA_OPEN";
 
-    if (fd->perm == ADIO_PERM_NULL) {
-        old_mask = umask(022);
-        umask(old_mask);
-        perm = old_mask ^ 0666;
-    } else
-        perm = fd->perm;
-
-    amode = 0;
-    if (fd->access_mode & ADIO_CREATE)
-        amode = amode | O_CREAT;
-    if (fd->access_mode & ADIO_RDONLY)
-        amode = amode | O_RDONLY;
-    if (fd->access_mode & ADIO_WRONLY)
-        amode = amode | O_WRONLY;
-    if (fd->access_mode & ADIO_RDWR)
-        amode = amode | O_RDWR;
-    if (fd->access_mode & ADIO_EXCL)
-        amode = amode | O_EXCL;
-
-#ifdef ADIOI_MPE_LOGGING
-    MPE_Log_event(ADIOI_MPE_open_a, 0, NULL);
-#endif
-    fd->fd_sys = open(fd->filename, amode, perm);
-#ifdef ADIOI_MPE_LOGGING
-    MPE_Log_event(ADIOI_MPE_open_b, 0, NULL);
-#endif
-    fd->fd_direct = -1;
-
-    if ((fd->fd_sys != -1) && (fd->access_mode & ADIO_APPEND)) {
-#ifdef ADIOI_MPE_LOGGING
-        MPE_Log_event(ADIOI_MPE_lseek_a, 0, NULL);
-#endif
-        fd->fp_ind = fd->fp_sys_posn = lseek(fd->fd_sys, 0, SEEK_END);
-#ifdef ADIOI_MPE_LOGGING
-        MPE_Log_event(ADIOI_MPE_lseek_b, 0, NULL);
-#endif
-    }
+    JBatch batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
+    JObject* object = j_object_new("ad", "julea");
+    j_object_create(object, batch);
+    j_batch_execute(batch);
+ 
+    fd->fs_ptr = object;
 
     if (fd->fd_sys == -1) {
         *error_code = ADIOI_Err_create_code(myname, fd->filename, errno);
