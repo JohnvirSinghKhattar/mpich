91568992f0a5c65b8b22bba96769cf6ba081aed6
Jan Harder <jha@greenhouse.media>
2020-04-22 13:43:31 +0200

    Update ad_julea_read.c
    Add pointer for JBatch

--- src/mpi/romio/adio/ad_julea/ad_julea_read.c
+++ src/mpi/romio/adio/ad_julea/ad_julea_read.c
@@ -30,7 +30,7 @@ void ADIOI_JULEA_ReadContig(ADIO_File fd, void *buf, int count,
     MPI_Type_size_x(datatype, &datatype_size);
     len = datatype_size * (ADIO_Offset) count;
 
-    JBatch batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
+    JBatch* batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
     j_object_read(fd->fs_ptr, ad_julea, len + 1, 0, &nbytes, batch);
     j_batch_execute(batch);
 }
