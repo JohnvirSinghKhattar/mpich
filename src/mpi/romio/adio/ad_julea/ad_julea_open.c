b95b297ab981b0d4bd1289c1612e88d15ae73e05
JohnvirSinghKhattar <36770911+JohnvirSinghKhattar@users.noreply.github.com>
2020-04-24 13:04:47 +0200

    Update ad_julea_open.c

--- src/mpi/romio/adio/ad_julea/ad_julea_open.c
+++ src/mpi/romio/adio/ad_julea/ad_julea_open.c
@@ -11,7 +11,7 @@ void ADIOI_JULEA_Open(ADIO_File fd, int *error_code)        //Open Methode laut 
 {
     static char myname[] = "ADIOI_JULEA_OPEN";              
 
-    JBatch batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT); //Erstellung eines Batch
+    JBatch* batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT); //Erstellung eines Batch
     JObject* object = j_object_new("ad", fd->filename); //Erstellung Object
                                                         //filename über die übergebene File
     j_object_create(object, batch);                     //Objekt wird auf Festplatte erstellt
