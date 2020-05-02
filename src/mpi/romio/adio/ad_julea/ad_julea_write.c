0ba5bab77d8be701d171b22e98d0629c28e4e3fc
JohnvirSinghKhattar <36770911+JohnvirSinghKhattar@users.noreply.github.com>
2020-05-02 13:59:29 +0200

    Update ad_julea_write.c

--- src/mpi/romio/adio/ad_julea/ad_julea_write.c
+++ src/mpi/romio/adio/ad_julea/ad_julea_write.c
@@ -26,7 +26,7 @@ void ADIOI_JULEA_WriteContig(ADIO_File fd, const void *buf, int count,
     } 
     MPI_Type_size_x(datatype, &datatype_size);
     len = datatype_size * (ADIO_Offset) count;
-    guint64* bytes_written;
+    guint64* bytes_written = 0;
     JBatch* batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
     j_object_write(fd->fs_ptr, buf, len, offset, bytes_written, batch);
     j_batch_execute(batch);
